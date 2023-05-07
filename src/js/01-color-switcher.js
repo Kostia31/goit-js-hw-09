4be9c9d6dddbedd8cf20dcdede37acb9388c12d7
Kostia31 <kostyapodnebesnuy@gmail.com>
2023-05-08 00:41:44 +0300

    add color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+  body: document.body,
+};
 
+class Coloraizer {
+  constructor() {
+    this.idInterval = 'null';
+  }
+  onChangeBodyColor() {
+    this.idInterval = setInterval(() => {
+      refs.body.style.backgroundColor = getRandomHexColor();
+      refs.start.disabled = true;
+    }, 1000);
+  }
+
+  ofChangeBodycolor() {
+    clearInterval(this.idInterval);
+    refs.start.disabled = false;
+  }
+}
+const colorSwitch = new Coloraizer();
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+
+refs.stop.addEventListener('click', colorSwitch.ofChangeBodycolor.bind(Coloraizer));
+refs.start.addEventListener('click', colorSwitch.onChangeBodyColor.bind(Coloraizer));
