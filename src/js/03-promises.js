c2c0d93d395b7c24a8f3958ebc128fe3d3a37aa4
Kostia31 <kostyapodnebesnuy@gmail.com>
2023-05-14 23:55:20 +0300

    add task #3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', getInputValue);
+
+function getInputValue(e) {
+  e.preventDefault();
+  const result = {};
+  const formData = new FormData(form);
+  formData.forEach((value, name) => {
+    result[name] = value;
+  });
+  const { delay, step, amount } = result;
+  let steps = Number(delay);
+  for (let i = 0; i < amount; i += 1) {
+    steps += Number(step);
+    createPromise(amount, steps)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
